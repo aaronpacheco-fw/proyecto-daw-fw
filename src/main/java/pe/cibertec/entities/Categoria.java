3bbc034d3c70baa96e7172d34229978ed267d646
AARON PACHECO <aarongamer32pro@gmail.com>
2026-08-20 00:02:35 -0500

    entidad categoria modificada

--- src/main/java/pe/cibertec/entities/Categoria.java
+++ src/main/java/pe/cibertec/entities/Categoria.java
@@ -2,6 +2,8 @@ package pe.cibertec.entities;
 
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
@@ -11,6 +13,9 @@ public class Categoria {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @NotBlank (message = "El nombre de la categoría es obligatorio")
+    @Size(max = 80, message = "El nombre de la categoría no puede superar los 80 caracteres")
     @Column(nullable = false, unique = true, length = 80)
     private String nombre;
 }
